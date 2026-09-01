ef0dee273bb6cbc40c535c997bfeaab83f8a3e07
jalexisgomezm-collab <jalexisgomezm@gmail.com>
2026-09-01 10:11:51 -0500

    Create page.tsx

---
+++ app/(app)/proveedores/nuevo/page.tsx
@@ -0,0 +1,10 @@
+import ProveedorForm from "@/components/ProveedorForm";
+
+export default function NuevoProveedorPage() {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold mb-6">Nuevo proveedor</h1>
+      <ProveedorForm />
+    </div>
+  );
+}
