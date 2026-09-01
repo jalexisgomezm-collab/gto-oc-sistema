bd16947adb08777287c6112c9f86b841db108b43
jalexisgomezm-collab <jalexisgomezm@gmail.com>
2026-09-01 10:15:19 -0500

    Create empresa.ts

---
+++ lib/empresa.ts
@@ -0,0 +1,19 @@
+export const EMPRESA = {
+  nombreLegal: "GTO PERU SOCIEDAD ANÓNIMA CERRADA",
+  ruc: "20519998786",
+  domicilioFiscal:
+    "CAL. GERMAN SCHEREIBER NRO. 276 INT. T301 URB. SANTA ANA (ENTRE CUADRA 1 Y 2 DE CANAVAL Y MOREYRA), SAN ISIDRO - LIMA - LIMA",
+  sedeOperativa:
+    "AV. VARIANTE UCHUMAYO KM. 5.5 LT. 657 INT. A1, SEC. ALTO CURAL, SACHACA - AREQUIPA",
+  correos: "servicios@gtoperu.com | administracion@gtoperu.com",
+  celulares: "921 502 895 / 932 439 863",
+  web: "www.gtoperu.com",
+  direccionCorta: "Variante de Uchumayo km 5.5, Sachaca - Arequipa - Perú",
+  whatsapp: "(+51) 932 439 863"
+};
+
+export const VERDE_HEX = "02934E";
+export const GRIS_ZEBRA_HEX = "F2F2F2";
+export const VERDE_CLARO_HEX = "EAF7EF";
+export const GRIS_TEXTO_HEX = "555555";
+export const FUENTE = "Calibri";
